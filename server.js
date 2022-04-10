ea7c43818c65b02090ef5600d1bb5e97b7c48b62
Debidatta Pattanayak <patnayak@clofus.com>
2022-04-10 20:13:46 +0530

    command change

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/hello-world'));
 
 app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/index.html');
+  res.status(200).sendFile(__dirname + '/dist/hello-world/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
